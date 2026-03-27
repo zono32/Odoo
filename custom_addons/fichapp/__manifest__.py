43cf5a287e68fa93206eb9e08b81d6e4b3c64142
zono32 <masterzono32@gmail.com>
2026-03-27 13:32:47 +0100

    añadir enlace a fichapp

---
+++ custom_addons/fichapp/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Fichapp Integration',
+    'version': '1.0',
+    'summary': 'Acceso directo a mi App de Fichajes',
+    'category': 'Human Resources',
+    'author': 'Kinso',
+    'depends': ['base'],
+    'data': [
+        'views/fichapp_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'icon': '/fichapp/static/description/icon.png', # Opcional si añades un icono luego
+}
